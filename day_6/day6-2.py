1dfee09a59ef93eff7691f428b3f2533135a12e8
alex-roosma-usafa <alexander.roosma@afacademy.af.edu>
2021-12-06 10:48:53 -0700

    day 6 - more pythonic versions

--- day_6/day6-2.py
+++ day_6/day6-2.py
@@ -1,4 +1,6 @@
-MATURATION_DAYS = 256
+from collections import Counter
+
+MATURATION_DAYS = 9
 SIM_LEN = 256
 
 file = open("day_6_input.txt", "r")
@@ -6,36 +8,22 @@ data = file.read().strip().split(",")
 
 # pairs of start end pairs
 fish_ages = [int(i) for i in data]
+fish_counter = Counter(fish_ages)
 
-print(fish_ages)
+# initialize missing values
+for day in range(MATURATION_DAYS):
+    if day not in fish_counter.keys():
+        fish_counter[day] = 0
 
-count_at_day = [0] * MATURATION_DAYS
-
-for age in fish_ages:
-    count_at_day[age] += 1
-
-print(count_at_day)
-
+# run thru fish generation simulation
 for day in range(SIM_LEN):
-    count_changes = [0] * MATURATION_DAYS
-
-    count_at_zero = count_at_day[0]
-
-    for age in range(1,MATURATION_DAYS):
-        count_changes[age-1] = count_at_day[age]
-
-    count_changes[6] += count_at_zero
-    count_changes[8] = count_at_zero
-
-    print(f"****** Day {day}")
-    # print(count_changes)
-
-    count_at_day = count_changes.copy()
-
-
+    count_at_zero = fish_counter[0]
     
-total = 0
-for curr in count_at_day:
-    total += curr
-print(f"Total fish: {total}")
+    for age in range(1,MATURATION_DAYS):
+        fish_counter[age-1] = fish_counter[age]
+
+    fish_counter[6] += count_at_zero
+    fish_counter[8] = count_at_zero
+
+print(f"Counter total: {sum(fish_counter.values())}")
 file.close()
