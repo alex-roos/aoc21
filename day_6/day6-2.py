fabe82f6d033053c7ef72615fe52e363c6a39fc2
Alex Roosma <alexander.roosma@afacademy.af.edu>
2021-12-07 20:22:53 -0700

    Cleaned day 6 files

--- day_6/day6-2.py
+++ day_6/day6-2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 file = open("day_6_input.txt", "r")
 data = file.read().strip().split(",")
 
@@ -19,32 +18,4 @@ for i in range(top_level, bottom_level + 1):
 
 print(min(move_costs))
 
-=======
-from collections import Counter
-
-MATURATION_DAYS = 9
-SIM_LEN = 256
-
-# data prep
-file = open("day_6_input.txt", "r")
-fish_ages = [int(i) for i in file.read().strip().split(",")]
-
-# initialize count values, including those for age with no count
-fish_counter = Counter(fish_ages)
-for day in range(MATURATION_DAYS):
-    if day not in fish_counter.keys():
-        fish_counter[day] = 0
-
-# run thru fish generation simulation
-for day in range(SIM_LEN):
-    count_at_zero = fish_counter[0]
-    
-    for age in range(1,MATURATION_DAYS):
-        fish_counter[age-1] = fish_counter[age]
-
-    fish_counter[6] += count_at_zero
-    fish_counter[8] = count_at_zero
-
-print(f"Counter total: {sum(fish_counter.values())}")
->>>>>>> de7baeb91be0205c810c4ae3e86f0f8cef6afe5d
 file.close()
